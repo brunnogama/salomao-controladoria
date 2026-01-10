30071aa311c6953f9178dc93767697880b916a8f
Bruno Gama <30611127+brunnogama@users.noreply.github.com>
2026-01-10 14:33:20 -0300

    Create DashboardLayout.tsx

---
+++ src/layouts/DashboardLayout.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { Sidebar } from '../components/Sidebar';
+
+export function DashboardLayout() {
+  return (
+    <div className="flex min-h-screen bg-gray-50">
+      <Sidebar />
+      <main className="flex-1 ml-64 p-8">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
