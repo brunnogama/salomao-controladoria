13df2bbafc4439c5a7aeed37f6e55ed0486b773c
Bruno Gama <30611127+brunnogama@users.noreply.github.com>
2026-01-10 14:44:21 -0300

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { menuItems } from '../config/menuConfig';
 import { History, Settings, LogOut, UserCircle } from 'lucide-react';
